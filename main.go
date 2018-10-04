10c84f4408e2802d1364ccd632501114c1fed6de
lihaolu2 <lihaolu2@illinois.edu>
2018-10-03 19:27:23 -0500

    Base framework for MP2

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
+// refer to https://varshneyabhi.wordpress.com/2014/12/23/simple-udp-clientserver-in-golang/
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+	"server"
+)
 
+// This function will register and initiate server
 func main() {
-	fmt.Println("Hello Golang!")
+	// parse argument
+	configFilePath := flag.String("c", "./config.json", "Config file path")
+	port := flag.Int("p", 8080, "Port number")
+	IP := flag.String("ip", "0.0.0.0", "IP address")
+
+	flag.Parse()
+
+	// load config file
+	configFile, e := ioutil.ReadFile(*configFilePath)
+	if e != nil {
+		log.Fatalf("File error: %v\n", e)
+	}
+
+	// Class for server
+	s := newServer()
+	s.loadConfigFromJSON(configFile)
+
+	s.setIP(*IP)
+	s.setPort(*port)
+	s.setFilePath(server.getFilePath())
+
+	fmt.Printf("Starting server on IP: %s and port: %d", *IP, *port)
+
+	go server.FailureDetection()
+	go server.ServerLoop()
 }
---
+++ model.go
@@ -0,0 +1,9 @@
+package model
+
+// NodeConfig Structure of node config
+type NodeConfig struct {
+	ID      int    `json:"id"`
+	IP      string `json:"ip"`
+	Port    int    `json:"port"`
+	LogPath string `json:"log_path"`
+}
