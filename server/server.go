143a22c4bafebdbdd44ab1d27a2e76620c7af95f
lihaolu2 <lihaolu2@illinois.edu>
2018-10-06 18:14:03 -0500

    fix

--- server/server.go
+++ server/server.go
@@ -301,11 +301,11 @@ func (s *Server) JoinToGroup() error {
 
 	buf = []byte{}
 	_, _, err = conn.ReadFromUDP(buf)
+	log.Printf("JoinToGroup: ReadFromUDP buf: %s", buf)
 	if err != nil {
 		return errors.New("unable to read from udp conn")
 	}
 
-	log.Printf("JoinToGroup: ReadFromUDP buf: %s", buf)
 	// buf: messageMemList:s.ID:ip-ts_inc:ip-ts_inc:...
 	bufList := bytes.Split(buf, []byte(":"))
 	if len(bufList[0]) > 0 && bufList[0][0] == byte(messageShowMemList) {
